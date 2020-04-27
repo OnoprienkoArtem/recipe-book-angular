f295cc5690aed02a705d653c01e399b77d5ba87c
OnoprienkoArtem <o.artem.e@gmail.com>
2020-04-27 21:52:01 +0300

    link in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +9,9 @@ export class HeaderComponent implements OnInit {
 
   collapsed = true;
 
-  @Output() featureSelected = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSelect(recipe: string) {
-    this.featureSelected.emit(recipe);
-  }
 }
