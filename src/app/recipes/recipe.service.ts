f8e9fc18da7a7570656b04fe2ba97ed30f3aa745
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-05 22:51:43 +0300

    set empty array if fetching date without ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,38 +11,40 @@ export class RecipeService {
 
   recipesChanged = new Subject<Recipe[]>();
 
-  recipes: Recipe[] = [
-    {
-      name: 'Tasty Schnitzel',
-      description: 'A super tasty Schnitzel - just awesome!',
-      imagePath: 'https://media-cdn.tripadvisor.com/media/photo-s/11/bb/4c/46/chicken-schnitzel-fries.jpg',
-      ingredients: [
-        {
-          name: 'Meat',
-          amount: 1,
-        },
-        {
-          name: 'French Fries',
-          amount: 20,
-        }
-      ],
-    },
-    {
-      name: 'Big Fat Burger',
-      description: 'What else you need to say?',
-      imagePath: 'https://images-na.ssl-images-amazon.com/images/I/71WZQEddn4L._SL1500_.jpg',
-      ingredients: [
-        {
-          name: 'Buns',
-          amount: 2,
-        },
-        {
-          name: 'Meat',
-          amount: 1,
-        },
-      ],
-    }
-  ];
+  // recipes: Recipe[] = [
+  //   {
+  //     name: 'Tasty Schnitzel',
+  //     description: 'A super tasty Schnitzel - just awesome!',
+  //     imagePath: 'https://media-cdn.tripadvisor.com/media/photo-s/11/bb/4c/46/chicken-schnitzel-fries.jpg',
+  //     ingredients: [
+  //       {
+  //         name: 'Meat',
+  //         amount: 1,
+  //       },
+  //       {
+  //         name: 'French Fries',
+  //         amount: 20,
+  //       }
+  //     ],
+  //   },
+  //   {
+  //     name: 'Big Fat Burger',
+  //     description: 'What else you need to say?',
+  //     imagePath: 'https://images-na.ssl-images-amazon.com/images/I/71WZQEddn4L._SL1500_.jpg',
+  //     ingredients: [
+  //       {
+  //         name: 'Buns',
+  //         amount: 2,
+  //       },
+  //       {
+  //         name: 'Meat',
+  //         amount: 1,
+  //       },
+  //     ],
+  //   }
+  // ];
+
+  recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) { }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,11 +19,19 @@ export class DataStorageService {
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
 
-    return this.http.put(`${this.url}recipes.json`, recipes).subscribe(res => console.log(res));
+    return this.http.put(`${this.url}recipes.json`, recipes)
+      .subscribe(res => console.log(res));
   }
 
   fetchRecipes() {
     this.http.get<Recipe[]>(`${this.url}recipes.json`)
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
