1dbf4e5ce8046fd7a956061eeaf006dabd545e6b
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-05 22:43:04 +0300

    fetch data from firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +18,13 @@ export class DataStorageService {
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
 
-    return this.http.put(`${this.url}recipes.json`, recipes).subscribe(res => {
-      console.log(res);
-    });
+    return this.http.put(`${this.url}recipes.json`, recipes).subscribe(res => console.log(res));
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>(`${this.url}recipes.json`)
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
