f4dfbc5cd432c87d7eb71fc961228b9cf45756d7
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-09 23:24:44 +0300

    redirect to recipes when success login or signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,7 +13,10 @@ export class AuthComponent implements OnInit {
   isLoginMode = true;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -41,8 +45,9 @@ export class AuthComponent implements OnInit {
 
     authObs.subscribe(
       res => {
-        this.authService.isLoader$.next(false);
         console.log(res);
+        this.authService.isLoader$.next(false);
+        this.router.navigate(['/recipes']);
       },
       errorMessage => {
         this.error = errorMessage;
