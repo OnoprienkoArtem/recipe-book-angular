70af92ec8990b45af87879d91cd2950b638c8280
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-12 23:40:58 +0300

    implement auto login

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -2,16 +2,18 @@ export class User {
   constructor(
     public email: string,
     public id: string,
-    public token: string,
-    private tokenExpirationDate: Date,
+    // tslint:disable-next-line:variable-name
+    private _token: string,
+    // tslint:disable-next-line:variable-name
+    private _tokenExpirationDate: Date,
   ) {
   }
 
-  get getToken() {
-    if (!this.tokenExpirationDate || new Date() > this.tokenExpirationDate) {
+  get token() {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
       return null;
     }
 
-    return this.token;
+    return this._token;
   }
 }
