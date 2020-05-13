d453f5cadf15809b783a0c914360e35e10611c7f
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-13 23:49:57 +0300

    add autologout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -70,6 +70,8 @@ export class AuthService {
 
     if (loadedUser.token) {
       this.user$.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
   }
 
@@ -93,6 +95,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user$.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
