0cb10fb2111eb20b4ef4f88680436ed0ea6ca6dd
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-15 22:38:22 +0300

    create reducer for shopping list

---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,16 @@
+const initialState = {
+  ingredients: [
+    {
+      name: 'Apples',
+      amount: 5
+    },
+    {
+      name: 'Tomatoes',
+      amount: 10
+    }
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action) {
+
+}
