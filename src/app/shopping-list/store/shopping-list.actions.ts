994182690770754e4e5e06607bdaaeed07381011
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-18 23:29:12 +0300

    create actions for upd and del ingredient

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,9 @@ import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
+
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -16,4 +19,17 @@ export class AddIngredients implements Action {
   constructor(public payload: Ingredient[]) {}
 }
 
-export  type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredients implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(public payload: { index: number, ingredient: Ingredient }) {}
+}
+
+export class DeleteIngredients implements Action {
+  readonly type = DELETE_INGREDIENTS;
+
+  constructor(public payload: number) {}
+}
+
+
+export type ShoppingListActions = AddIngredient | AddIngredients | UpdateIngredients | DeleteIngredients;
