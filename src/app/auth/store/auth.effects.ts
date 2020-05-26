dc13234923ae2934f132c091b50c8c9b832f7e59
OnoprienkoArtem <o.artem.e@gmail.com>
2020-05-26 23:03:04 +0300

    work with auth effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,14 +1,40 @@
-import { Actions, ofType } from '@ngrx/effects';
-
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import * as AuthActions from './auth.actions';
+import { AuthResponseData } from '../auth.service';
+import { of } from 'rxjs';
 
 export class AuthEffects {
 
+  apiKey = '';
+
+  @Effect()
   authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authDate: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apiKey}`,
+        {
+          email: authDate.payload.email,
+          password: authDate.payload.password,
+          returnSecureToken: true
+        }
+      ).pipe(
+        catchError(error => {
+          of();
+        }),
+        map(resData => {
+          of();
+        }),
+      );
+    }),
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient
+  ) {}
 
 
 }
