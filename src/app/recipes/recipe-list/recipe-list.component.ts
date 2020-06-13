f8a2cbc493121a06ce9611eba14256f96b409199
OnoprienkoArtem <o.artem.e@gmail.com>
2020-06-14 00:00:37 +0300

    get recipes in list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,6 +3,10 @@ import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-list',
@@ -17,17 +21,19 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   constructor(
     private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>,
   ) { }
 
   ngOnInit(): void {
-    this.subscription = this.recipeService.recipesChanged
+    this.subscription = this.store
+    .select('recipes')
+    .pipe(map(recipesState => recipesState.recipes))
     .subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
-    );
-    this.recipes = this.recipeService.getRecipes();
+    );    
   }
 
   onNewRecipe() {
