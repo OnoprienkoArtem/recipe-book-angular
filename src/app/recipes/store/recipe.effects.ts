bdb0fefc6ebeabae824f7c880bf290b5369eeb54
OnoprienkoArtem <o.artem.e@gmail.com>
2020-06-18 23:58:27 +0300

    work with fetch recipes effect

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,11 +1,33 @@
 import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap, map } from 'rxjs/operators';
+import { Recipe } from '../recipe.model';
+import { HttpClient } from '@angular/common/http';
 
 import * as RecipesActions from './recipe.actions';
 
 export class RecipeEffects {
-    @Effect()
-    fetchRecipes = this.actions$.pipe(ofType(RecipesActions.FETCH_RECIPES));
 
-    constructor(private actions$: Actions) {}
+    url = '';
+
+    @Effect()
+    fetchRecipes = this.actions$.pipe(
+        ofType(RecipesActions.FETCH_RECIPES),
+        switchMap(() => {
+            return this.http.get<Recipe[]>(`${this.url}recipes.json`)
+        }),
+        map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }),
+        map(recipes => {
+            return new RecipesActions.SetRecipes(recipes);
+        }),
+    );
+
+    constructor(
+        private actions$: Actions,
+        private http: HttpClient,
+    ) {}
 } 
  
