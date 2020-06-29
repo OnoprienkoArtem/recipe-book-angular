9920736828a74cdcd9d42ed2f3136916b7929388
OnoprienkoArtem <o.artem.e@gmail.com>
2020-06-29 23:37:43 +0300

    add common angular module in recipes module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes.component';
@@ -15,6 +18,11 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
         RecipeItemComponent,
         RecipesStartComponent,
         RecipeEditComponent,
+    ],
+    imports: [
+        RouterModule,
+        CommonModule,
+        ReactiveFormsModule,
     ],
     exports: [
         RecipesComponent,
