0d7d8431cbabbb70ef89b453f26d73ceb987f03f
OnoprienkoArtem <o.artem.e@gmail.com>
2020-07-04 23:25:19 +0300

    add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AuthComponent } from './auth/auth.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
@@ -19,16 +17,15 @@ import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
-    FormsModule,
-    ReactiveFormsModule,
+    AuthModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
